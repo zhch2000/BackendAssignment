12684f693f43cc93a54bec4e94387532f32ef062
zhch2000 <zhch2000@hotmail.com>
2019-08-19 23:16:42 -0600

    added json parse working with: go run main.go {\"action\":\"jump\",\"time\":100} {\"action\":\"run\",\"time\":75} {\"action\":\"jump\",\"time\":200}

--- main.go
+++ main.go
@@ -12,33 +12,36 @@ import (
     "os" 
     "bytes"   
     "strconv"
+    "encoding/json"                                   
 )
 
 var actions = make(map[string][2]int)
-
-func addAction(action string, ch chan<-string) {
-    fmt.Println(actions[action])
-    if actions[action][0] > 0 {
-        array := [2]int{actions[action][0] + 1, actions[action][1] + 100}
-        actions[action] = array
-    } else {
-        array := [2]int{1, 100}
-        actions[action] = array           
-    }
-    ch <- fmt.Sprintf("%d %d %s", actions[action][0], actions[action][1], action)
+type ActionJson struct {
+  Action string
+  Time int
 }
 
-func main() {
-    ch := make(chan string)
-    for _,action := range os.Args[1:] {
-        go addAction(action, ch)
+func addAction(actionJson string, ch chan<-string) {
+
+    //actionJson := `{"action": "jump","time":100}`
+	var actionJ ActionJson 
+	json.Unmarshal([]byte(actionJson), &actionJ)
+	fmt.Printf("Action: %s, Time: %d\n", actionJ.Action, actionJ.Time)
+    //if err!=nil{
+        //fmt.Print("Error:",err)
+    //}
+    //json.Unmarshal(action, &conf)
+    //fmt.Println("%s, %s", conf.action, conf.time)
+    
+    //fmt.Println(actions[actionJ.Action])
+    if actions[actionJ.Action][0] > 0 {
+        array := [2]int{actions[actionJ.Action][0] + 1, actions[actionJ.Action][1] + actionJ.Time}
+        actions[actionJ.Action] = array
+    } else {
+        array := [2]int{1, actionJ.Time}
+        actions[actionJ.Action] = array           
     }
-    for range os.Args[1:] {
-        fmt.Println(<-ch)
-    }
-    //fmt.Printf("%s elapsed\n", action)
-    go getStats(ch)
-    fmt.Println(<-ch)
+    ch <- fmt.Sprintf("%d %d %s", actions[actionJ.Action][0], actions[actionJ.Action][1], actionJ.Action)
 }
 
 func getStats(ch chan<-string) {
@@ -48,10 +51,24 @@ func getStats(ch chan<-string) {
         buf.WriteString("{\"action\":\"")
         buf.WriteString(key)
         buf.WriteString("\", \"avg\":")
-        buf.WriteString(strconv.FormatFloat(float64(action[1])/float64(action[0]+1),'f',2,64))             
+        buf.WriteString(strconv.FormatFloat(float64(action[1])/float64(action[0]),'f',2,64))  // 2 decimal Avg            
         buf.WriteString("},\n")
     }
     result := buf.String()
+    
     ch <- fmt.Sprintf(result)
 }
 
+func main() {
+    ch := make(chan string)
+    
+    for _,action := range os.Args[1:] {
+        go addAction(action, ch)
+    }
+    for range os.Args[1:] {
+        fmt.Println(<-ch)
+    }
+    
+    go getStats(ch)
+    fmt.Println(<-ch)
+}
